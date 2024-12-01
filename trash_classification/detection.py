5f6404b8172c86497d92031c63698ad1672430d1
angjustinl <96008766+ANGJustinl@users.noreply.github.com>
2024-12-01 19:48:32 +0800

    classify code

--- trash_classification/detection.py
+++ trash_classification/detection.py
@@ -3,42 +3,39 @@ from logger import logger
 from picamera2 import Picamera2
 from ultralytics import YOLO
 
+from uart import UARTCommunication
 from vision import *
 
 
-def main():
+def classify(image, confidence_threshold=0.7):
     # Load pre-trained Model
     model = YOLO("models/trash_openvino", task="detect")
     logger.info("Pre-trained YOLOv11s Model loaded")
 
-    device = torch.device(
-        "cuda" if torch.cuda.is_available() else "cpu"
-    )  # 可惜没cuda
-    logger.info(f"Device using: {device}")
-
     # Initialize camera
     picam2 = Picamera2()
     picam2.start()
-    while True:
-        # read frame
-        # ret, frame = camera.read()
-        image = picam2.capture_array("main")
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    
+    image = picam2.capture_array("main")
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        # Perform object detection on an image
-        result_img, _ = Predict_and_detect(
-            model, image, classes=[], min_conf=0.5, device=device
-        )
+    # Perform object detection on an image and get the class of the detected object
+    results = Predict(model, image, stream=False)
 
-        # Display results
-        cv2.imshow("YOLOv11 Inference", result_img)
-        key = cv2.waitKey(1)
-        if key == 32:  # 空格
-            break
-
+    # Loop through the results and print the class and confidence
+    confident_objects = []
+    for result in results:
+        for box in result.boxes:
+            confidence = box.conf.tolist()[0]
+            if confidence < confidence_threshold:
+                continue
+            label = int(box.cls[0])
+            caption = f"{result.names[label]} {confidence:.2f}"
+            logger.info(caption)
+            confident_objects.append(caption)
     picam2.close()
-    cv2.destroyAllWindows()
+    return confident_objects
 
 
 if __name__ == "__main__":
-    main()
+    print(classify())
