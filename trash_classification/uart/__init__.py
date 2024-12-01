17369dcecad84b1d68509dedc1fec4c82dcd8e23
ANGJustinl <96008766+ANGJustinl@users.noreply.github.com>
2024-12-01 12:38:51 +0000

    Classify code update

--- trash_classification/uart/__init__.py
+++ trash_classification/uart/__init__.py
@@ -1,10 +1,11 @@
-import time
+# ANG 24.12.1
 import serial
 import logging
 
 # 设置日志记录
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 class UARTCommunication:
     """
